010219e612a9e8d57e59366e8aee7703461dbe31
balodi <balodi@gmail.com>
2018-02-01 08:48:30 +0530

    handler - test case passed to check the value returned by server with filecontents

--- handler/handler.test.js
+++ handler/handler.test.js
@@ -1,8 +1,15 @@
 const server = require('./handler.js');
 const Hapi = require('hapi');
+const fs = require('fs');
 
 describe('test for server', () => {
   test('server should be a server object', () => {
     expect(server).toBeInstanceOf(Hapi.Server);
+  });
+  test('should return hello abc', () => {
+    server.inject('/', (res) => {
+      const fileContent = fs.readFile('/Users/sahilbalodi/Desktop/exercise/handler/index.html');
+      expect(res.result).toBe(fileContent.toString());
+    });
   });
 });
