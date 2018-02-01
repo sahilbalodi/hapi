1653441a3641aec8feb443591d08309650f0b1ee
balodi <balodi@gmail.com>
2018-02-01 13:25:39 +0530

    view - testcase added to check if server returns correct content

--- views/view.js
+++ views/view.js
@@ -1,9 +1,28 @@
 const Hapi = require('hapi');
+const Vision = require('vision');
+const Path = require('path');
+const handlebars = require('handlebars');
 
 const server = new Hapi.Server();
 server.connection({
-  port: Number(process.argv[4] || 8050),
+  port: Number(process.argv[4] || 8010),
   host: 'localhost',
+});
+server.register(Vision, (err) => {
+  if (err) throw err;
+});
+server.views({
+  engines: {
+    html: handlebars,
+  },
+  path: Path.join(__dirname, 'template'),
+});
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: {
+    view: 'index.html',
+  },
 });
 server.start((error) => {
   if (error) { throw error; }
--- views/view.test.js
+++ views/view.test.js
@@ -1,8 +1,18 @@
 const server = require('./view.js');
 const Hapi = require('hapi');
+const fs = require('fs');
 
 describe('test server ', () => {
   test('should return server object', () => {
     expect(server).toBeInstanceOf(Hapi.Server);
+  });
+});
+describe('test server ', () => {
+  test('should should return html', (done) => {
+    const fileContent = fs.readFileSync('/Users/sahilbalodi/Desktop/exercise/views/template/index.html', 'utf-8');
+    server.inject('/?name=sahil', (res) => {
+      expect(res.result).toBe(fileContent);
+      done();
+    });
   });
 });
