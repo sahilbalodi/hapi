ec1ec941240c6eca0754ef689b9261f02c1ef14d
balodi <balodi@gmail.com>
2018-02-01 21:49:57 +0530

    verify - test passed to return null if argument is integer

--- helping/helping.js
+++ helping/helping.js
@@ -1,12 +1,33 @@
 const Hapi = require('hapi');
+const Vision = require('vision');
+const handlebars = require('handlebars');
+const Path = require('path');
 
 const server = new Hapi.Server();
 
 server.connection({
-  port: Number(process.argv[2] || 8000),
+  port: Number(process.argv[4] || 8000),
   host: 'localhost',
 });
-server.start((error) => {
+server.register(Vision, (error) => {
   if (error) { throw error; }
-  console.log('server start');
 });
+server.views({
+  engines: {
+    html: handlebars,
+  },
+  helpersPath: Path.join(__dirname, 'helpers'),
+  path: Path.join(__dirname, 'helpers'),
+});
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: { view: 'helper' },
+});
+if (!module.parent) {
+  server.start((error) => {
+    if (error) { throw error; }
+    console.log('server start');
+  });
+}
+module.exports = server;
--- helping/verify.js
+++ helping/verify.js
@@ -1,6 +1,8 @@
 module.exports = (firstParameter, secondParameter) => {
   if ((firstParameter === undefined) && (secondParameter === undefined)) {
     return null;
+  } else if ((typeof (firstParameter) === 'number') || (typeof (secondParameter) === 'number')) {
+    return null;
   }
   return true;
 };
