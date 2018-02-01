440b43ccc5ef663e1cd886e59f631b05acad1f14
balodi <balodi@gmail.com>
2018-02-01 07:17:53 +0530

    server - passed testcase - to test if a server object is returned

--- hellohapi.test.js
+++ hellohapi.test.js
@@ -9,13 +9,16 @@
 // }).catch((error) => { console.log(error); });
 // });
 // });
-const server = require('./abc.js');
+const Hapi = require('hapi');
+const server = require('./hello_hapi.js');
 
 describe('test for server ', () => {
   test('should return hello hapi', () => {
     server.inject('/', (res) => {
       expect(res.result).toBe('hello hapi');
-      console.log('hello hapi');
     });
+  });
+  test('server should be a server object', () => {
+    expect(server).toBeInstanceOf(Hapi.Server);
   });
 });
