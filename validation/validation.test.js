eacfa5e9644d11e9c4e77f85f21dd3377e261b0c
balodi <balodi@gmail.com>
2018-02-02 08:26:56 +0530

    validation- testcase passed to test if server response is right

--- validation/validation.test.js
+++ validation/validation.test.js
@@ -7,14 +7,28 @@ describe('server', () => {
   });
 });
 describe('server', () => {
-  test('server should return 200 status code if valid params are passed in url', () => {
+  test('server should return 200 status code if valid params are passed in url', (done) => {
     server.inject('/chicken/abcd', (response) => {
       expect(response.statusCode).toBe(200);
+      done();
     });
   });
-  test('server should return 400 status code if invalid params are passed in url', () => {
+  test('server should return 400 status code if invalid params are passed in url', (done) => {
     server.inject('/chicken/abcdefgh', (response) => {
       expect(response.statusCode).toBe(400);
+      done();
+    });
+  });
+  test('server returns correct reponse data', (done) => {
+    server.inject('/chicken/abcde', (response) => {
+      expect(response.result).toBe('chicken abcde');
+      done();
+    });
+  });
+  test('server returns correct reponse data', (done) => {
+    server.inject('/chicken/abcdefgh', (response) => {
+      expect(response.result.error).toBe('Bad Request');
+      done();
     });
   });
 });
